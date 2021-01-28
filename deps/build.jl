5e3b37350f3b6c687e40b4501350f5c6d109bec3
rymanderson <rymanderson@gmail.com>
2021-01-28 11:29:46 -0700

    still having trouble finding OpenMP

--- deps/build.jl
+++ deps/build.jl
@@ -11,7 +11,7 @@ NOTE: for Mac users, the following environment variables must be set before `FLO
 
 ```bash
 export CMAKE_C_COMPILER_JLENV="/usr/local/Cellar/llvm/10.0.1/bin/clang"
-export CMAKE_CXX_COMPILER_JLENV="/usr/local/Cellar/llvm/10.0.1/bin/clang++" 
+export CMAKE_CXX_COMPILER_JLENV="/usr/local/Cellar/llvm/10.0.1/bin/clang++"
 export OPENMP_LIBRARIES_JLENV="/usr/local/Cellar/llvm/10.0.1/lib"
 export OPENMP_INCLUDES_JLENV="/usr/local/Cellar/llvm/10.0.1/include"
 export JlCxx_DIR=/Users/randerson/.julia/artifacts/6017255205dc4fbf4d962903a855a0c631f092dc/
@@ -39,45 +39,69 @@ if Sys.isapple()
 	CMAKE_CXX_COMPILER_JLENV = joinpath(useversion,"bin/clang++")
 	OPENMP_LIBRARIES_JLENV = joinpath(useversion,"lib")
 	OPENMP_INCLUDES_JLENV = joinpath(useversion,"include")
-	
+    if !(isfile(CMAKE_C_COMPILER_JLENV) && isfile(CMAKE_CXX_COMPILER_JLENV) && isdir(OPENMP_LIBRARIES_JLENV) && isdir(OPENMP_INCLUDES_JLENV))
+        throw("The following environment variables could not be set:\n\tCMAKE_C_COMPILER_JLENV\n\tCMAKE_CXX_COMPILER_JLENV\n\tOPENMP_LIBRARIES_JLENV\n\tOPENMP_INCLUDES_JLENV\n\nSet these manually and rebuild `FLOWExaFMM`")
+    end
 	# set JLCxx prefix path variable
 	JlCxx_DIR = CxxWrap.prefix_path()
 
 	# write to file
 	buildcontents = open(joinpath(packagedir, "build.sh"), "r") do io
 		return read(io, String)
-	end
-	
-	open(joinpath(packagedir, "build.sh"), "w") do io
-		write(io, "# set environment variables for use with CxxWrap\n")
-
-		write(io, "export JlCxx_DIR=$(JlCxx_DIR)\n")
-        write(io, "\n")
+    end
+    build_cutoff_i = findfirst(isequal('@'), buildcontents)
+    buildcontents = buildcontents[build_cutoff_i+1:end]
+    open(joinpath(packagedir, "build.sh"), "w") do io
+        write(io, "# set environment variables to use LLVM compilers and libraries\n")
+        write(io, "export CMAKE_C_COMPILER_JLENV=$(CMAKE_C_COMPILER_JLENV)\n")
+        write(io, "export CMAKE_CXX_COMPILER_JLENV=$(CMAKE_CXX_COMPILER_JLENV)\n")
+        write(io, "export OPENMP_LIBRARIES_JLENV=$(OPENMP_LIBRARIES_JLENV)\n")
+        write(io, "export OPENMP_INCLUDES_JLENV=$(OPENMP_INCLUDES_JLENV)\n\n")
+        write(io, "# set environment variables for use with CxxWrap\n")
+        write(io, "export JlCxx_DIR=$(JlCxx_DIR)\n\n# @")
         write(io, buildcontents)
-	end
-    
+    end
+
+    makecontents = open(joinpath(packagedir, "make.sh"), "r") do io
+		return read(io, String)
+    end
+    make_cutoff_i = findfirst(isequal('@'), makecontents)
+    makecontents = makecontents[make_cutoff_i+1:end]
+    open(joinpath(packagedir, "make.sh"), "w") do io
+        write(io, "# set environment variables to use LLVM compilers and libraries\n")
+        write(io, "export CMAKE_C_COMPILER_JLENV=$(CMAKE_C_COMPILER_JLENV)\n")
+        write(io, "export CMAKE_CXX_COMPILER_JLENV=$(CMAKE_CXX_COMPILER_JLENV)\n")
+        write(io, "export OPENMP_LIBRARIES_JLENV=$(OPENMP_LIBRARIES_JLENV)\n")
+        write(io, "export OPENMP_INCLUDES_JLENV=$(OPENMP_INCLUDES_JLENV)\n\n")
+        write(io, "# set environment variables for use with CxxWrap\n")
+        write(io, "export JlCxx_DIR=$(JlCxx_DIR)\n\n# @")
+        write(io, buildcontents)
+    end
+
     # println("CMAKE_C_COMPILER_JLENV = ", CMAKE_C_COMPILER_JLENV)
     # println("CMAKE_CXX_COMPILER_JLENV = ", CMAKE_CXX_COMPILER_JLENV)
     # println("OPENMP_LIBRARIES_JLENV = ", OPENMP_LIBRARIES_JLENV)
     # println("OPENMP_INCLUDES_JLENV = ", OPENMP_INCLUDES_JLENV)
-	if isfile(CMAKE_C_COMPILER_JLENV) && isfile(CMAKE_CXX_COMPILER_JLENV) && isdir(OPENMP_LIBRARIES_JLENV) && isdir(OPENMP_INCLUDES_JLENV)
-	    buildcontents = open(joinpath(packagedir, "build.sh"), "r") do io
-		    return read(io, String)
-	    end
-	    open(joinpath(packagedir, "build.sh"), "w") do io
-		    write(io, "# set environment variables to use LLVM compilers and libraries\n")
-		    write(io, "export CMAKE_C_COMPILER_JLENV=$(CMAKE_C_COMPILER_JLENV)\n")
-		    write(io, "export CMAKE_CXX_COMPILER_JLENV=$(CMAKE_CXX_COMPILER_JLENV)\n")
-		    write(io, "export OPENMP_LIBRARIES_JLENV=$(OPENMP_LIBRARIES_JLENV)\n")
-		    write(io, "export OPENMP_INCLUDES_JLENV=$(OPENMP_INCLUDES_JLENV)\n")
-		    write(io, buildcontents)
-	    end
-	else	
-		println("The following environment variables could not be set:\n\tCMAKE_C_COMPILER_JLENV\n\tCMAKE_CXX_COMPILER_JLENV\n\tOPENMP_LIBRARIES_JLENV\n\tOPENMP_INCLUDES_JLENV\n\nSet these manually and rebuild `FLOWExaFMM`")
-	end	
-		
+	# if isfile(CMAKE_C_COMPILER_JLENV) && isfile(CMAKE_CXX_COMPILER_JLENV) && isdir(OPENMP_LIBRARIES_JLENV) && isdir(OPENMP_INCLUDES_JLENV)
+	#     buildcontents = open(joinpath(packagedir, "build.sh"), "r") do io
+	# 	    return read(io, String)
+	#     end
+	#     open(joinpath(packagedir, "build.sh"), "w") do io
+	# 	    write(io, "# set environment variables to use LLVM compilers and libraries\n")
+	# 	    write(io, "export CMAKE_C_COMPILER_JLENV=$(CMAKE_C_COMPILER_JLENV)\n")
+	# 	    write(io, "export CMAKE_CXX_COMPILER_JLENV=$(CMAKE_CXX_COMPILER_JLENV)\n")
+	# 	    write(io, "export OPENMP_LIBRARIES_JLENV=$(OPENMP_LIBRARIES_JLENV)\n")
+	# 	    write(io, "export OPENMP_INCLUDES_JLENV=$(OPENMP_INCLUDES_JLENV)\n")
+	# 	    write(io, buildcontents)
+	#     end
+	# else
+	# 	println("The following environment variables could not be set:\n\tCMAKE_C_COMPILER_JLENV\n\tCMAKE_CXX_COMPILER_JLENV\n\tOPENMP_LIBRARIES_JLENV\n\tOPENMP_INCLUDES_JLENV\n\nSet these manually and rebuild `FLOWExaFMM`")
+	# end
+
 end
 
-mycommand = joinpath(packagedir, "build.sh")
-run(`sh -c $mycommand`)
+buildcommand = joinpath(packagedir, "build.sh")
+makecommand = joinpath(packagedir, "make.sh")
+# run(`sh -c $buildcommand`)
+# run(`sh -c $makecommand`)
 println("Finished building FLOWExaFMM")
--- make.sh
+++ make.sh
@@ -5,17 +5,33 @@ export OPENMP_LIBRARIES_JLENV=/usr/local/Cellar/llvm/10.0.1/lib
 export OPENMP_INCLUDES_JLENV=/usr/local/Cellar/llvm/10.0.1/include
 
 # set environment variables for use with CxxWrap
-export JlCxx_DIR=/Users/randerson/.julia/artifacts/6017255205dc4fbf4d962903a855a0c631f092dc/
+export JlCxx_DIR=/Users/randerson/.julia/artifacts/6017255205dc4fbf4d962903a855a0c631f092dc
+
+# @
 
 THIS_DIR=$(pwd)
-BUILD_DIR=${THIS_DIR}/build
-rm -rf ${BUILD_DIR}
-mkdir ${BUILD_DIR}
-cd ${BUILD_DIR}
-echo "Configuring build..."
-echo "  Source path: ${THIS_DIR}/deps/3d"
-CC=clang CXX=clang++ cmake ${THIS_DIR}/deps/3d
-echo "Making files..."
-cmake --build .
-echo "Copying dynamic library..."
-cp lib/libfmm.dylib ../src/fmm.dylib
+SRC_DIR=deps
+COMPILE_DIR=build
+SAVE_DIR=src
+
+echo "Removing existing build"
+rm -rf $COMPILE_DIR
+rm $SAVE_DIR/fmm.so
+
+echo "Copying files"
+mkdir $COMPILE_DIR
+cp -r $SRC_DIR/* $COMPILE_DIR/
+
+echo "Configuring build"
+cd $COMPILE_DIR/
+# ./configure --enable-single
+./configure
+
+echo "Compiling 3d"
+cd 3d
+make
+
+cd $THIS_DIR
+cp $COMPILE_DIR/3d/fmm $SAVE_DIR/fmm.so
+
+echo "Done!"
