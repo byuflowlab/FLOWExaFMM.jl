27ecc16b7f6f33c760640229a5950ceef7e7c6d7
rymanderson <rymanderson@gmail.com>
2021-01-28 10:30:19 -0700

    attempt to restore lost files

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+# set environment variables to use LLVM compilers and libraries
+export CMAKE_C_COMPILER_JLENV=/usr/local/Cellar/llvm/10.0.1/bin/clang
+export CMAKE_CXX_COMPILER_JLENV=/usr/local/Cellar/llvm/10.0.1/bin/clang++
+export OPENMP_LIBRARIES_JLENV=/usr/local/Cellar/llvm/10.0.1/lib
+export OPENMP_INCLUDES_JLENV=/usr/local/Cellar/llvm/10.0.1/include
+# set environment variables for use with CxxWrap
+export JlCxx_DIR=/Users/randerson/.julia/artifacts/6017255205dc4fbf4d962903a855a0c631f092dc
+
 THIS_DIR=$(pwd)
 SRC_DIR=deps
 COMPILE_DIR=build
--- make.sh
+++ make.sh
@@ -1,4 +1,12 @@
+# set environment variables to use LLVM compilers and libraries
+export CMAKE_C_COMPILER_JLENV=/usr/local/Cellar/llvm/10.0.1/bin/clang
+export CMAKE_CXX_COMPILER_JLENV=/usr/local/Cellar/llvm/10.0.1/bin/clang++
+export OPENMP_LIBRARIES_JLENV=/usr/local/Cellar/llvm/10.0.1/lib
+export OPENMP_INCLUDES_JLENV=/usr/local/Cellar/llvm/10.0.1/include
+
+# set environment variables for use with CxxWrap
 export JlCxx_DIR=/Users/randerson/.julia/artifacts/6017255205dc4fbf4d962903a855a0c631f092dc/
+
 THIS_DIR=$(pwd)
 BUILD_DIR=${THIS_DIR}/build
 rm -rf ${BUILD_DIR}
