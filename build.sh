6d3751216df1f4a1e460926f184f5176563f86df
EdoAlvarezR <Edo.AlvarezR@gmail.com>
2021-08-24 13:12:55 -0600

    Move user-specific paths out of Makefile.in into build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
+# --------------- USER INPUTS --------------------------------------------------
+# JULIA_H must point to the directory that contains julia.h
+JULIA_H=/home/edoalvar/Programs/julia-1.6.1/include/julia
+
+# JLCXX_H must point to the directory that contains jlcxx/jlcxx.hpp from CxxWrap
+# NOTE: You can find this by typing `CxxWrap.prefix_path()` in the Julia REPL
+JLCXX_H=/home/edoalvar/.julia/artifacts/16e1de4679fb8520a8af4e6831c7c8e9893d18b4/include
+
+# Julia_LIB must point to the directory that contains libjulia.so.x
+JULIA_LIB=$JULIA_H/../../lib
+
+# JLCXX_LIB must point to the directory that contains libcxxwrap_julia.so.0.x.x
+JLCXX_LIB=$JLCXX_H/../lib
+
+
+# --------------- COMPILE CODE -------------------------------------------------
 THIS_DIR=$(pwd)
 SRC_DIR=deps
 COMPILE_DIR=build
@@ -18,7 +34,7 @@ cd $COMPILE_DIR/
 
 echo "Compiling 3d"
 cd 3d
-make
+make JULIA_H=$JULIA_H JLCXX_H=$JLCXX_H JULIA_LIB=$JULIA_LIB JLCXX_LIB=$JLCXX_LIB
 
 cd $THIS_DIR
 cp $COMPILE_DIR/3d/fmm $SAVE_DIR/fmm.so
