9deb2a165162f5a03d52a83be522d7c96d690f00
rymanderson <rymanderson@gmail.com>
2022-02-28 11:52:16 -0700

    add build script for mac without using CMake

--- build_macos.sh
+++ build_macos.sh
@@ -6,17 +6,19 @@ CC=/usr/local/opt/llvm/bin/clang
 CXX=/usr/local/opt/llvm/bin/clang++
 
 # JULIA_H must point to the directory that contains julia.h
-JULIA_H=/Applications/Julia-1.6.app/Contents/Resources/julia/include/julia
+JULIA_H=/Applications/Julia-1.7.app/Contents/Resources/julia/include/julia
 
 # JLCXX_H must point to the directory that contains jlcxx/jlcxx.hpp from CxxWrap
 # NOTE: You can find this by typing `CxxWrap.prefix_path()` in the Julia REPL
-JLCXX_H=/Users/randerson/.julia/artifacts/eb1ece2a20e3bd68968d861ee1e8e6a00d077d9e/include
+# JLCXX_H=/Users/randerson/.julia/artifacts/eb1ece2a20e3bd68968d861ee1e8e6a00d077d9e/include
+JLCXX_H=/Users/randerson/tmps/juliadev/libcxxwrap-julia/include
 
 # Julia_LIB must point to the directory that contains libjulia.so.x
 JULIA_LIB=$JULIA_H/../../lib
 
 # JLCXX_LIB must point to the directory that contains libcxxwrap_julia.so.0.x.x
-JLCXX_LIB=$JLCXX_H/../lib
+# JLCXX_LIB=$JLCXX_H/../lib
+JLCXX_LIB=/Users/randerson/tmps/juliadev/libcxxwrap-julia/build_llvm/lib
 
 # MPICXX path
 MPIHOME=/usr/local/bin/mpicxx
@@ -43,8 +45,8 @@ cp -r $SRC_DIR/* $COMPILE_DIR/
 
 echo "Configuring build"
 cd $COMPILE_DIR/
-./configure CXX=$CXX MPICXX=$MPIHOME LDFLAGS=$LDFLAGS --enable-single
-# CC=$CC
+./configure CC=$CC CXX=$CXX MPICXX=$MPIHOME LDFLAGS=$LDFLAGS --disable-mpi --disable-omp
+# --enable-single
 # ./configure
 
 echo "Compiling 3d"
